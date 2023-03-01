5ec19b8ee8fbf92eb88e9a9b464f4965bcc5b725
Hajar Mohamed <hajr.m7md@gmail.com>
2023-03-01 18:46:24 +0300

    display the price in html

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,6 @@ exports.index_get = (req,res) => {
     res.render('home/index')
 }
 
-exports.index_get = (req,res) => {
-    res.render('home/another')
-}
+// exports.index_get = (req,res) => {
+//     res.render('home/another')
+// }
